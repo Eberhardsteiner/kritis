b00a245fc70d8fd85d2553b58d527a79a3edfcf7
Eberhardsteiner <Eberhard.steiner@uvm-institut.de>
2026-04-09 08:59:58 +0200

    2

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useRef, useState } from 'react';
 import { ActiveViewPanel } from './components/ActiveViewPanel';
 import { Sidebar } from './components/Sidebar';
 import { ProjectTopbar } from './components/ProjectTopbar';
+import { getReadOnlyHint, useAppDerivedState } from './hooks/useAppDerivedState';
 import {
   exportActionPlanAsCsv,
   exportAssessmentAsJson,
@@ -14,51 +15,13 @@ import {
   exportStakeholderRegisterAsCsv,
 } from './lib/exporters';
 import {
-  buildEffectiveModuleCatalog,
   builtInModuleContainers,
   builtInModules,
-  getActionTemplatesForModule,
-  getAuditChecklistForModule,
-  getDependencyTemplatesForModule,
-  getDocumentFoldersForModule,
-  getEvidenceTemplatesForModule,
-  getExerciseTemplatesForModule,
-  getKritisRequirementsForModule,
-  getModuleById,
   getModuleByIdFromCatalog,
-  getProcessTemplatesForModule,
-  getQuestionsForModule,
-  getRoleTemplatesForModule,
-  getScenarioTemplatesForModule,
   parseAndValidateModule,
 } from './lib/moduleRegistry';
-import {
-  assessKritisApplicability,
-  buildBenchmarkSnapshot,
-  buildLinkedCountMap,
-  computeScoreSnapshot,
-  getActionSummary,
-  getAuditFindingSummary,
-  getCertificationProgress,
-  getChecklistProgress,
-  getEvidenceSummary,
-  getGovernanceSummary,
-  getRequirementProgress,
-  getResilienceSummary,
-} from './lib/scoring';
 import { getAccessProfile } from './data/workspaceBase';
-import {
-  buildRegimeSummaries,
-  filterActiveChecklist,
-  filterActiveRequirements,
-  getRegimeDefinitions,
-  normalizeRegulatoryProfile,
-} from './lib/regulatory';
-import {
-  buildDeadlineSummary,
-  buildDocumentLibrarySummary,
-  getDocumentFolderSuggestions,
-} from './lib/workspace';
+import { normalizeRegulatoryProfile } from './lib/regulatory';
 import { clearAuthToken, loadAuthToken, loadState, saveAuthToken, saveState } from './lib/storage';
 import {
   createApiClient,
@@ -146,8 +109,6 @@ import type {
   IntegritySummary,
   ObservabilitySummary,
   RegulatoryProfile,
-  RegulatoryRegimeSummary,
-  RegulatoryRegimeDefinition,
   GermanyRegimeId,
   RegimeScopeStatus,
   JobRunSummary,
@@ -158,7 +119,6 @@ import type {
   RequirementStatus,
   ReleaseGateItem,
   RestoreDrillSummary,
-  ResilienceSummary,
   ReviewPlan,
   ServerHealth,
   SnapshotInfo,
@@ -1083,151 +1043,59 @@ export default function App() {
   const suppressNextServerSyncRef = useRef(false);
   const lastSyncedPayloadRef = useRef<string>(serializeServerPayload(state));
 
-  const effectiveModuleCatalog = useMemo(
-    () => buildEffectiveModuleCatalog(state.uploadedModules, moduleRegistryEntries),
-    [state.uploadedModules, moduleRegistryEntries],
-  );
-
-  const currentModule = useMemo(
-    () => getModuleByIdFromCatalog(state.selectedModuleId, effectiveModuleCatalog) ?? effectiveModuleCatalog[0] ?? builtInModules[0],
-    [state.selectedModuleId, effectiveModuleCatalog],
-  );
-
-  const questions = useMemo(() => getQuestionsForModule(currentModule), [currentModule]);
-  const questionLookup = useMemo(
-    () => new Map(questions.map((question) => [question.id, question])),
-    [questions],
-  );
-
-  const requirements = useMemo(
-    () => getKritisRequirementsForModule(currentModule),
-    [currentModule],
-  );
-  const requirementLookup = useMemo(
-    () => new Map(requirements.map((requirement) => [requirement.id, requirement])),
-    [requirements],
-  );
-
-  const actionTemplates = useMemo(
-    () => getActionTemplatesForModule(currentModule),
-    [currentModule],
-  );
-  const evidenceTemplates = useMemo(
-    () => getEvidenceTemplatesForModule(currentModule),
-    [currentModule],
-  );
-  const processTemplates = useMemo(
-    () => getProcessTemplatesForModule(currentModule),
-    [currentModule],
-  );
-  const dependencyTemplates = useMemo(
-    () => getDependencyTemplatesForModule(currentModule),
-    [currentModule],
-  );
-  const scenarioTemplates = useMemo(
-    () => getScenarioTemplatesForModule(currentModule),
-    [currentModule],
-  );
-  const exerciseTemplates = useMemo(
-    () => getExerciseTemplatesForModule(currentModule),
-    [currentModule],
-  );
-  const roleTemplates = useMemo(
-    () => getRoleTemplatesForModule(currentModule),
-    [currentModule],
-  );
-  const auditChecklist = useMemo(
-    () => getAuditChecklistForModule(currentModule),
-    [currentModule],
-  );
-  const regulatoryProfile = useMemo(
-    () => normalizeRegulatoryProfile(state.regulatoryProfile),
-    [state.regulatoryProfile],
-  );
-  const regimeDefinitions = useMemo<RegulatoryRegimeDefinition[]>(
-    () => getRegimeDefinitions(regulatoryProfile.jurisdiction),
-    [regulatoryProfile.jurisdiction],
-  );
-  const activeRequirements = useMemo(
-    () => filterActiveRequirements(requirements, regulatoryProfile),
-    [requirements, regulatoryProfile],
-  );
-  const activeAuditChecklist = useMemo(
-    () => filterActiveChecklist(auditChecklist, regulatoryProfile),
-    [auditChecklist, regulatoryProfile],
-  );
-  const regimeSummaries = useMemo<RegulatoryRegimeSummary[]>(
-    () => buildRegimeSummaries({
-      requirements,
-      requirementStates: state.requirementStates,
-      checklist: auditChecklist,
-      checklistStates: state.auditChecklistStates,
-      regulatoryProfile,
-    }),
-    [requirements, state.requirementStates, auditChecklist, state.auditChecklistStates, regulatoryProfile],
-  );
-
-  const scoreSnapshot = useMemo(
-    () => computeScoreSnapshot(questions, state.answers, currentModule),
-    [questions, state.answers, currentModule],
-  );
-
-  const currentActionItems = useMemo(
-    () => state.actionItems.filter((item) => item.moduleId === currentModule.id),
-    [state.actionItems, currentModule.id],
-  );
-  const currentEvidenceItems = useMemo(
-    () => state.evidenceItems.filter((item) => item.moduleId === currentModule.id),
-    [state.evidenceItems, currentModule.id],
-  );
-  const currentStakeholders = useMemo(
-    () => state.stakeholders.filter((item) => item.moduleId === currentModule.id),
-    [state.stakeholders, currentModule.id],
-  );
-  const currentSites = useMemo(
-    () => state.sites.filter((item) => item.moduleId === currentModule.id),
-    [state.sites, currentModule.id],
-  );
-  const currentAssets = useMemo(
-    () => state.assets.filter((item) => item.moduleId === currentModule.id),
-    [state.assets, currentModule.id],
-  );
-  const currentBusinessProcesses = useMemo(
-    () => state.businessProcesses.filter((item) => item.moduleId === currentModule.id),
-    [state.businessProcesses, currentModule.id],
-  );
-  const currentDependencies = useMemo(
-    () => state.dependencies.filter((item) => item.moduleId === currentModule.id),
-    [state.dependencies, currentModule.id],
-  );
-  const currentScenarios = useMemo(
-    () => state.scenarios.filter((item) => item.moduleId === currentModule.id),
-    [state.scenarios, currentModule.id],
-  );
-  const currentExercises = useMemo(
-    () => state.exercises.filter((item) => item.moduleId === currentModule.id),
-    [state.exercises, currentModule.id],
-  );
-  const currentHardeningChecks = useMemo(
-    () => state.hardeningChecks.filter((item) => item.moduleId === currentModule.id),
-    [state.hardeningChecks, currentModule.id],
-  );
-  const currentRunbooks = useMemo(
-    () => state.runbooks.filter((item) => item.moduleId === currentModule.id),
-    [state.runbooks, currentModule.id],
-  );
-  const currentReleaseGates = useMemo(
-    () => state.releaseGates.filter((item) => item.moduleId === currentModule.id),
-    [state.releaseGates, currentModule.id],
-  );
-  const currentFindings = useMemo(
-    () => state.auditFindings.filter((item) => item.moduleId === currentModule.id),
-    [state.auditFindings, currentModule.id],
-  );
-  const documentFolders = useMemo(
-    () => getDocumentFolderSuggestions(getDocumentFoldersForModule(currentModule), currentEvidenceItems),
-    [currentModule, currentEvidenceItems],
-  );
+  const {
+    effectiveModuleCatalog,
+    currentModule,
+    questions,
+    questionLookup,
+    requirements,
+    requirementLookup,
+    actionTemplates,
+    evidenceTemplates,
+    processTemplates,
+    dependencyTemplates,
+    scenarioTemplates,
+    exerciseTemplates,
+    roleTemplates,
+    auditChecklist,
+    regulatoryProfile,
+    regimeDefinitions,
+    activeRequirements,
+    activeAuditChecklist,
+    regimeSummaries,
+    scoreSnapshot,
+    currentActionItems,
+    currentEvidenceItems,
+    currentStakeholders,
+    currentSites,
+    currentAssets,
+    currentBusinessProcesses,
+    currentDependencies,
+    currentScenarios,
+    currentExercises,
+    currentHardeningChecks,
+    currentRunbooks,
+    currentReleaseGates,
+    currentFindings,
+    documentFolders,
+    requirementProgress,
+    kritisApplicability,
+    actionSummary,
+    evidenceSummary,
+    documentLibrarySummary,
+    governanceSummary,
+    resilienceSummary,
+    deadlineSummary,
+    benchmarkSnapshot,
+    checklistProgress,
+    findingSummary,
+    certificationProgress,
+    questionActionCounts,
+    questionEvidenceCounts,
+    requirementActionCounts,
+    requirementEvidenceCounts,
+    attachmentCount,
+  } = useAppDerivedState({ state, moduleRegistryEntries });
   const activeUser = useMemo(() => {
     if (authSession) {
       return state.users.find((item) => item.id === authSession.userId)
@@ -1911,122 +1779,7 @@ export default function App() {
     }
   }
 
-  const requirementProgress = useMemo(
-    () => getRequirementProgress(activeRequirements, state.requirementStates),
-    [activeRequirements, state.requirementStates],
-  );
-
-  const kritisApplicability = useMemo(
-    () => assessKritisApplicability(state.companyProfile, currentModule, regulatoryProfile.jurisdiction),
-    [state.companyProfile, currentModule],
-  );
-
-  const actionSummary = useMemo(
-    () => getActionSummary(currentActionItems),
-    [currentActionItems],
-  );
-  const evidenceSummary = useMemo(
-    () => getEvidenceSummary(currentEvidenceItems),
-    [currentEvidenceItems],
-  );
-  const documentLibrarySummary = useMemo(
-    () => buildDocumentLibrarySummary(currentEvidenceItems),
-    [currentEvidenceItems],
-  );
-  const governanceSummary = useMemo(
-    () => getGovernanceSummary(currentStakeholders, currentSites, currentAssets, state.reviewPlan),
-    [currentStakeholders, currentSites, currentAssets, state.reviewPlan],
-  );
-  const resilienceSummary = useMemo<ResilienceSummary>(
-    () => getResilienceSummary(currentBusinessProcesses, currentDependencies, currentScenarios, currentExercises),
-    [currentBusinessProcesses, currentDependencies, currentScenarios, currentExercises],
-  );
-  const deadlineSummary = useMemo(
-    () => buildDeadlineSummary({
-      actionItems: currentActionItems,
-      evidenceItems: currentEvidenceItems,
-      exercises: currentExercises,
-      reviewPlan: state.reviewPlan,
-      complianceCalendar: state.complianceCalendar,
-      applicability: kritisApplicability,
-      regulatoryProfile,
-    }),
-    [currentActionItems, currentEvidenceItems, currentExercises, state.reviewPlan, state.complianceCalendar, kritisApplicability, regulatoryProfile],
-  );
-  const benchmarkSnapshot = useMemo(
-    () => buildBenchmarkSnapshot(currentModule, state.companyProfile.employees, scoreSnapshot),
-    [currentModule, state.companyProfile.employees, scoreSnapshot],
-  );
-  const checklistProgress = useMemo(
-    () => getChecklistProgress(activeAuditChecklist, state.auditChecklistStates),
-    [activeAuditChecklist, state.auditChecklistStates],
-  );
-  const findingSummary = useMemo(
-    () => getAuditFindingSummary(currentFindings),
-    [currentFindings],
-  );
-  const certificationProgress = useMemo(
-    () => getCertificationProgress(
-      state.certificationState,
-      requirementProgress.score,
-      evidenceSummary.coverage,
-    ),
-    [state.certificationState, requirementProgress.score, evidenceSummary.coverage],
-  );
-
-  const questionActionCounts = useMemo(
-    () => buildLinkedCountMap(currentActionItems, 'relatedQuestionIds'),
-    [currentActionItems],
-  );
-  const questionEvidenceCounts = useMemo(
-    () => buildLinkedCountMap(currentEvidenceItems, 'relatedQuestionIds'),
-    [currentEvidenceItems],
-  );
-  const requirementActionCounts = useMemo(
-    () => buildLinkedCountMap(currentActionItems, 'relatedRequirementIds'),
-    [currentActionItems],
-  );
-  const requirementEvidenceCounts = useMemo(
-    () => buildLinkedCountMap(currentEvidenceItems, 'relatedRequirementIds'),
-    [currentEvidenceItems],
-  );
-  const attachmentCount = useMemo(
-    () => state.evidenceItems.filter((item) => item.attachment || item.serverAttachment).length,
-    [state.evidenceItems],
-  );
-  const readOnlyHint = useMemo(() => {
-    if (state.activeView === 'assessment' && !hasPermission('assessment_edit')) {
-      return 'Lesemodus: Für Analyse und Unternehmensprofil fehlen dem aktiven Profil Schreibrechte.';
-    }
-    if (state.activeView === 'measures' && !hasPermission('actions_edit') && !hasPermission('evidence_edit')) {
-      return 'Lesemodus: Für Maßnahmen und Evidenzen fehlen dem aktiven Profil Schreibrechte.';
-    }
-    if (state.activeView === 'resilience' && !hasPermission('governance_edit')) {
-      return 'Lesemodus: BIA, Abhängigkeiten, Szenarien und Übungen erfordern governance_edit.';
-    }
-    if (state.activeView === 'governance' && !hasPermission('governance_edit')) {
-      return 'Lesemodus: Governance- und Strukturänderungen sind für dieses Profil gesperrt.';
-    }
-    if (state.activeView === 'control' && !hasPermission('workspace_edit')) {
-      return 'Lesemodus: Nutzerverwaltung und Compliance-Kalender erfordern workspace_edit.';
-    }
-    if (state.activeView === 'modules' && !hasPermission('modules_manage')) {
-      return 'Lesemodus: Das Importieren oder Pflegen von Branchenmodulen ist für dieses Profil gesperrt.';
-    }
-    if (state.activeView === 'kritis' && !hasPermission('kritis_edit')) {
-      return 'Lesemodus: KRITIS-Bausteine, Audit-Checklist und Readiness-Stufen erfordern kritis_edit.';
-    }
-    if (state.activeView === 'report' && !hasPermission('reports_export')) {
-      return 'Hinweis: Exporte sind für dieses Profil deaktiviert.';
-    }
-    if (state.activeView === 'rollout' && !hasPermission('workspace_edit')) {
-      return 'Lesemodus: Go-Live-Plan, Härtung und Übergabegates erfordern workspace_edit.';
-    }
-    if (state.activeView === 'platform' && !hasPermission('workspace_edit')) {
-      return 'Hinweis: Server-Snapshots und Wiederherstellungen erfordern workspace_edit.';
-    }
-    return '';
-  }, [state.activeView, activeAccessProfile]);
+  const readOnlyHint = getReadOnlyHint(state.activeView, hasPermission);
 
   useEffect(() => {
     void loadStateFromServer();
